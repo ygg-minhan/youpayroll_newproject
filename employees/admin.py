159d9672068a8c1c2db966dbce8a96ad60e4be29
Abhinand S S <abhinand@yougotagift.com>
2024-06-11 10:46:07 +0530

    A celery task implemented in order to fetch payee details from Zoho People using hrm_id. Docker-compose file updated with necessary configuration.

--- employees/admin.py
+++ employees/admin.py
@@ -1,40 +1,11 @@
 import logging
 from django.contrib import admin
-from django.contrib import messages
 from .models import (Payee, TDS, Payment, BankDetails, PayRecordRegister,
                      PayRecord, BankDetailsAck)
 from employees.utils import restrict_queryset_by_group
-from zohopeople.utils import get_employees_details
-
+from .tasks import fetch_details
 
 logger = logging.getLogger(__name__)
-
-
-# Admin action to fetch payee details from zoho people
-@admin.action(description="Fetch payee details from Zoho people")
-def fetch_details(modeladmin, request, queryset):
-    for payee in queryset:
-        payee_id = payee.hrm_id
-        # Calling the function get_employee_details and return response
-        try:
-            response_data = get_employees_details(payee_id).json()
-            response_data_list = response_data["response"]["result"][0]
-        except Exception as e:
-            logger.warning(e)
-            response_data_list = {}
-
-        if response_data_list:
-            for i in response_data_list.values():
-                fetched_data = i[0]
-                payee.full_name = fetched_data["FirstName"] + \
-                                     " " + fetched_data["LastName"]
-                payee.email = fetched_data["EmailID"]
-                payee.pan_no = fetched_data["Pan_Number"]
-                payee.address = fetched_data["Permanent_Address"]
-                payee.date_of_joining = fetched_data["Dateofjoining"]
-                payee.save()
-                messages.success(request, "Payee details were "
-                                          "successfully fetched.")
 
 
 class PayeeAdmin(admin.ModelAdmin):
@@ -42,7 +13,10 @@ class PayeeAdmin(admin.ModelAdmin):
     readonly_fields = ["full_name", "email", "pan_no", "address",
                        "date_of_joining"]
     ordering = ("status",)
-    actions = [fetch_details]
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        fetch_details.delay(obj.hrm_id)
 
     def delete_queryset(self, request, queryset):
         queryset.update(is_deleted=True)
