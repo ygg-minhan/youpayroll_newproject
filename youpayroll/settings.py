766a501edf5a6ddd4200681ec6836620b20aafc9
Abhinand S S <abhinand@yougotagift.com>
2024-07-09 18:19:13 +0530

    YP-29: Dockerize YouPayroll with PostgreSQL database and set up docker-compose.yml

--- youpayroll/settings.py
+++ youpayroll/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'youpayroll.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASES_NAME'),
+        'USER': config('DATABASES_USER'),
+        'PASSWORD': config('DATABASES_PASSWORD'),
+        'HOST': config('DATABASES_HOST'),
+        'PORT': config('DATABASES_PORT')
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
