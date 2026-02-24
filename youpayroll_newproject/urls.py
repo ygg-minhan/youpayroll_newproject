9f87fcb202ec280f0db6341f0a5a1213ea047a34
Minhan <anshik@Minhan-MBP.local>
2026-02-24 10:46:51 +0530

    Rename admin URL and site branding to vinton-gray-cerf

--- youpayroll_newproject/urls.py
+++ youpayroll_newproject/urls.py
@@ -3,8 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = "vinton-gray-cerf"
+admin.site.site_title = "vinton-gray-cerf Portal"
+admin.site.index_title = "Welcome to vinton-gray-cerf"
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('vinton-gray-cerf/', admin.site.urls),
     path('api/', include('core.urls')),
     path('api/configs/', include('configs.urls')),
     path('api/payees/', include('payees.urls')),
